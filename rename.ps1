b40dfe35651cf846392ad3ae8639226edbee5244
Skillown <nonowenad@gmail.com>
2025-01-15 12:51:00 +0200

    add f7

--- rename.ps1
+++ rename.ps1
@@ -1,6 +1,6 @@
 $i = 1
-Get-ChildItem -Path "D:\Go\src\github.com\tiltuky.github.io\scripts\family" -File | Sort-Object Name | ForEach-Object {
-    $newName = "f$i" + $_.Extension
+Get-ChildItem -Path "D:\Go\src\github.com\tiltuky.github.io\photo\family\noname" -File | Sort-Object Name | ForEach-Object {
+    $newName = "$i" + $_.Extension
     Rename-Item $_.FullName -NewName $newName
     $i++
 }
